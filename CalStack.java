ab37839fb3b447c5f3cfb21c0ae82ea2efd1f099
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-21 17:12:12 -0400

    Update CalStack.java

--- CalStack.java
+++ CalStack.java
@@ -1,19 +1,35 @@
-class CalStack {
-    LinkedList list = new LinkedList();
-    
-    public void push(String data) {
-        list.add(data);
+/*
+*Ethan Chang
+*echang28@u.rochester.edu
+*
+*07/21/2024: Creating Infix Calculator
+*Code adapted from Lab 2
+*/
+class CalStack 
+{
+    CalLinkedList list = new CalLinkedList(); //initialize new linked list
+    public void push(String data) 
+    {
+        list.insert(data); //insert into stack
     }
     
-    public String pop() {
-        if (list.isEmpty()) {
-            return null;
-        }
+    public String pop() 
+    {
+      if (list.isEmpty()) 
+      {
+        return null;
+      }
         // Implement a method to remove the last added item (not shown here for brevity)
-        return list.removeLast();
+        return list.removeFirst();
     }
     
-    public boolean isEmpty() {
-        return list.isEmpty();
+    public boolean isEmpty() 
+    {
+      return list.isEmpty(); //checks if stack is empty
+    }
+    
+    public String peek() 
+    {
+      return list.peekFirst(); //gives value of top of stack
     }
 }
