7c15b40c2ccbcaef0e0a1ea587d2a7f0dabb9fb8
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-21 22:08:20 -0400

    Update CalMain.java

--- CalMain.java
+++ CalMain.java
@@ -4,6 +4,8 @@
 *
 *07/21/2024: Creating Infix Calculator
 */
+import java.util.Scanner;
+
 public class CalMain 
 {
   /*main user interface*/
@@ -11,9 +13,16 @@ public class CalMain
   {
     CalSolution calc = new CalSolution(); //new CalSolution
     Scanner scan = new Scanner(System.in); //Defines scanner to read user input
-    System.out.println("Please enter an infix equation to evaluate:"); //prompts user for input
-    String userinp = scan.nextLine(); //reads user input
-    String result = calc.calculate(userinp); //calculates value from user input
-    System.out.println("Calculated value: " + result); //prints out calculated value
+    try
+    {
+      System.out.println("Please enter an infix equation to evaluate:"); //prompts user for input
+      String userinp = scan.nextLine(); //reads user input
+      String result = calc.calculate(userinp); //calculates value from user input
+      System.out.println("Calculated value: " + result); //prints out calculated value
+    }
+    finally 
+    {
+      scan.close(); //close scan when finished
+    }
   }
 }
